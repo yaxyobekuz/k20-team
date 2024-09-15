72944421597b013cd26f414d30158d265e0e56b3
yaxyobekb <pubgn9642@gmail.com>
2024-09-15 21:31:47 +0500

    update `main layout`

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,48 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// swiper
+import "swiper/css";
+import "swiper/css/effect-fade";
+import { Autoplay, EffectFade } from "swiper/modules";
+
 // components
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+
+// data
+import layoutSwiperSlides from "../data/layoutSwiperSlides";
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-red-50">
-      <Header />
-      <main className="grow">
-        <Outlet />
-      </main>
-      <Footer />
+    <div className="min-h-screen">
+      <div className="flex flex-col justify-between relative z-10 site-overlay min-h-screen">
+        {/* header */}
+        <Header />
+
+        {/* main */}
+        <main>
+          <Outlet />
+        </main>
+      </div>
+
+      {/* background */}
+      <Swiper
+        effect="fade"
+        autoplay={{ delay: 5000 }}
+        modules={[EffectFade, Autoplay]}
+        className="main-swiper fixed inset-0 z-0 size-full"
+      >
+        {layoutSwiperSlides.map((slide, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={slide.image}
+              className="size-full object-cover bg-gray-500"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
