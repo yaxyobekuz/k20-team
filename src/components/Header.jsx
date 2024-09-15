3e49459379b6765968aed837705e6df279f31da2
yaxyobekb <pubgn9642@gmail.com>
2024-09-15 21:27:13 +0500

    add header without responsive styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+// images
+import truckLogo from "../assets/images/logos/truck.svg";
+
+const Header = ({ className = "" }) => {
   return (
-    <header className="py-5">
+    <header className={`py-5 ${className}`}>
       <div className="container">
-        <img src={truckLogo} alt="" className="" />
+        <div className="flex items-center justify-between gap-5">
+          <nav className="flex items-center gap-12 animate__animated animate__fadeInDown">
+            {/* logo */}
+            <Link to="/" className="flex items-center gap-3.5">
+              <img
+                width={64}
+                height={64}
+                src={truckLogo}
+                alt="truck logo svg"
+                className="size-16"
+              />
+
+              {/* logo text */}
+              <h1 className="font-medium uppercase text-lg">K20 TEAM</h1>
+            </Link>
+
+            {/* nav list */}
+            <ul className="flex items-center gap-3.5">
+              <li>
+                <Link to="/" className="transition-colors hover:text-primary">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  className="transition-colors hover:text-primary"
+                >
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* header btn */}
+          <Link
+            to="/join"
+            className="animate__animated animate__fadeInRight bg-primary backdrop-blur px-10 py-2.5 text-secondary rounded-full font-medium transition-colors duration-300 hover:bg-primary/70"
+          >
+            Join us
+          </Link>
+        </div>
       </div>
     </header>
   );
