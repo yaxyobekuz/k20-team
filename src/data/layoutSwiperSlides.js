90bd88199f93e28f7912d0036b510b2ee2a2a0f4
yaxyobekb <pubgn9642@gmail.com>
2024-09-15 21:31:31 +0500

    add `main layout swiper slides` data

---
+++ src/data/layoutSwiperSlides.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        image: "https://w.wallhaven.cc/full/4v/wallhaven-4vopl0.jpg",
+    },
+    {
+        image: "https://wallpapers.com/images/hd/american-truck-simulator-oregon-road-81675mg1l57qyl8w.jpg",
+    },
+    {
+        image: "https://americantrucksimulator.com/images/bg_or.jpg",
+    },
+    {
+        image: "https://pbs.twimg.com/media/EkcW5YjW0AEFdr6.jpg:large",
+    },
+    {
+        image: "https://wallpapercave.com/wp/wp6956965.jpg",
+    },
+];
