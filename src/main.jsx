1bed63c90c21d6881dfc114f66289303a2e43129
yaxyobekb <pubgn9642@gmail.com>
2024-09-15 21:26:39 +0500

    add `animate css` to the site

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// components
 import App from "./App.jsx";
+
+// styles
+import "animate.css";
 import "./index.css";
 
+// render the site
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
