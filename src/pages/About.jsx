3b116e1d80c64bc1579d45e8de68ae623ea18560
yaxyobekb <pubgn9642@gmail.com>
2024-09-16 10:48:11 +0500

    add `responsive styles` to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,36 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const About = () => (
-  <div className="pt-20 pb-40">
-    <div className="container">
-      {/* page content */}
-      <div className="max-w-3xl space-y-8">
-        {/* page title */}
-        <h1 className="animate__animated animate__fadeInDown text-4xl font-medium">
-          About
-        </h1>
+  <div className="container">
+    {/* page content */}
+    <div className="max-w-xl space-y-8">
+      {/* page title */}
+      <h1 className="animate__animated animate__fadeInDown">About</h1>
 
-        {/* description */}
-        <p className="animate__animated animate__fadeInLeft text-lg text-gray-200">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam nam
-          accusantium hic? Harum delectus ducimus eum a suscipit, placeat
-          distinctio! Exercitationem non recusandae rem quam commodi, adipisci
-          aperiam quidem dicta harum, necessitatibus ipsa eligendi ab voluptate
-          incidunt provident ullam natus obcaecati dolorem omnis voluptatem
-          maiores enim amet? Doloribus praesentium inventore aliquid cumque
-          numquam id.
-        </p>
+      {/* description */}
+      <p className="animate__animated animate__fadeInLeft text-gray-200 sm:text-lg">
+        K20 Team is an auto transport logistics partner that provides a
+        customizable software solution built to fulfill all your car hauling
+        needs. Our tools are designed to maximize your businesses ability to
+        streamline, automate and organize your entire car hauling process from
+        start to finish.
+      </p>
 
-        {/* links wrapper */}
-        <div className="animate__animated animate__fadeInUp flex gap-5">
-          {/* join us */}
-          <Link
-            to="/join"
-            className="flex items-center justify-center w-40 bg-primary py-3 rounded-xl text-secondary font-medium transition-colors duration-300 hover:bg-primary/70"
-          >
-            Join us
-          </Link>
-        </div>
+      {/* links wrapper */}
+      <div className="animate__animated animate__fadeInUp flex gap-5">
+        {/* join us */}
+        <Link
+          to="/join"
+          className="flex items-center justify-center w-40 bg-primary py-3 rounded-lg text-secondary font-medium transition-colors duration-300 hover:bg-primary/70"
+        >
+          Join us
+        </Link>
       </div>
     </div>
   </div>
