415738c897321f0c21798e644cf2c90545c4c193
yaxyobekb <pubgn9642@gmail.com>
2024-09-16 11:58:10 +0500

    add `trucks background` images

--- src/data/layoutSwiperSlides.js
+++ src/data/layoutSwiperSlides.js
@@ -1,17 +1,23 @@
+import truck1Image from "../assets/images/backgrounds/truck1.jpg";
+import truck2Image from "../assets/images/backgrounds/truck2.jpg";
+import truck3Image from "../assets/images/backgrounds/truck3.jpg";
+import truck4Image from "../assets/images/backgrounds/truck4.jpg";
+import truck5Image from "../assets/images/backgrounds/truck5.jpg";
+
 export default [
     {
-        image: "https://w.wallhaven.cc/full/4v/wallhaven-4vopl0.jpg",
+        image: truck1Image
     },
     {
-        image: "https://wallpapers.com/images/hd/american-truck-simulator-oregon-road-81675mg1l57qyl8w.jpg",
+        image: truck2Image
     },
     {
-        image: "https://americantrucksimulator.com/images/bg_or.jpg",
+        image: truck3Image
     },
     {
-        image: "https://pbs.twimg.com/media/EkcW5YjW0AEFdr6.jpg:large",
+        image: truck4Image
     },
     {
-        image: "https://wallpapercave.com/wp/wp6956965.jpg",
+        image: truck5Image
     },
 ];
